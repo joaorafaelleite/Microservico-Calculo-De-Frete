dca29e3201897990d33a5cae7010d118bc86e590
João Rafael Leite <joaorafael.leite@hotmail.com>
2025-06-27 00:46:52 -0300

    feat: ajuste do controller e criacao dos casos de uso

--- ms-calculodefrete/src/main/java/com/itaulatam/ms_calculodefrete/api/FreteController.java
+++ ms-calculodefrete/src/main/java/com/itaulatam/ms_calculodefrete/api/FreteController.java
@@ -3,6 +3,7 @@ package com.itaulatam.ms_calculodefrete.api;
 import com.itaulatam.ms_calculodefrete.api.request.PedidoFreteRequestDto;
 import com.itaulatam.ms_calculodefrete.api.response.PedidoDeFreteResponseDto;
 import com.itaulatam.ms_calculodefrete.business.FreteService;
+import com.itaulatam.ms_calculodefrete.business.usecases.TipoDeTransporte;
 import com.itaulatam.ms_calculodefrete.infrastructure.interfaces.FreteStrategyInterface;
 import com.itaulatam.ms_calculodefrete.infrastructure.models.FreteContext;
 import com.itaulatam.ms_calculodefrete.infrastructure.models.FreteExpresso;
@@ -20,26 +21,15 @@ import java.util.List;
 @RequestMapping("/frete")
 public class FreteController
 {
-    private FreteContext freteContext;
-    private List<FreteStrategyInterface> freteStrategies;
     private FreteService freteService;
 
     @Autowired
-    public FreteController(FreteContext freteContext, List<FreteStrategyInterface> freteStrategies, FreteService freteService) {
-        this.freteContext = freteContext;
-        this.freteStrategies = freteStrategies;
+    public FreteController(FreteService freteService) {
         this.freteService = freteService;
     }
 
     @PostMapping("/calcular")
     public PedidoDeFreteResponseDto calcular(@RequestBody @Validated PedidoFreteRequestDto requestDto){
-        switch (requestDto.getTipoDeTransporte()) {
-            case NORMAL -> freteStrategies.add(0, new FreteNormal());
-            case EXPRESSO -> freteStrategies.add(0, new FreteExpresso());
-        }
-
-        freteContext.setFreteStrategy(freteStrategies.get(0));
-
-        return  freteService.calcular(requestDto, freteStrategies.get(0));
+        return  freteService.calcular(requestDto);
     }
 }
--- ms-calculodefrete/src/main/java/com/itaulatam/ms_calculodefrete/business/FreteService.java
+++ ms-calculodefrete/src/main/java/com/itaulatam/ms_calculodefrete/business/FreteService.java
@@ -3,6 +3,7 @@ package com.itaulatam.ms_calculodefrete.business;
 import com.itaulatam.ms_calculodefrete.api.converter.PedidoDeFreteConverter;
 import com.itaulatam.ms_calculodefrete.api.request.PedidoFreteRequestDto;
 import com.itaulatam.ms_calculodefrete.api.response.PedidoDeFreteResponseDto;
+import com.itaulatam.ms_calculodefrete.business.usecases.TipoDeTransporte;
 import com.itaulatam.ms_calculodefrete.infrastructure.exceptions.BusinessException;
 import com.itaulatam.ms_calculodefrete.infrastructure.models.FreteContext;
 import com.itaulatam.ms_calculodefrete.infrastructure.models.PedidoDeFrete;
@@ -12,17 +13,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FreteService {
+    private TipoDeTransporte tipoDeTransporte;
     private PedidoDeFreteConverter pedidoDeFreteConverter;
     private FreteContext freteContext;
 
     @Autowired
-    public FreteService(PedidoDeFreteConverter pedidoDeFreteConverter, FreteContext freteContext){
+    public FreteService(TipoDeTransporte tipoDeTransporte, PedidoDeFreteConverter pedidoDeFreteConverter, FreteContext freteContext){
+        this.tipoDeTransporte = tipoDeTransporte;
         this.pedidoDeFreteConverter = pedidoDeFreteConverter;
         this.freteContext = freteContext;
     }
 
-    public PedidoDeFreteResponseDto calcular(PedidoFreteRequestDto requestDto, FreteStrategyInterface freteStrategy){
+    public PedidoDeFreteResponseDto calcular(PedidoFreteRequestDto requestDto){
         try {
+            FreteStrategyInterface freteStrategy = tipoDeTransporte.selecionarFreteStrategy(requestDto);
+
             PedidoDeFrete pedidoDeFrete = pedidoDeFreteConverter.paraPedidoDeFrete(requestDto);
 
             freteStrategy.setPedidoFrete(pedidoDeFrete);
